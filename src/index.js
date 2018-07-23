d1052b73129a3be43cf25a71e0bfea942365c392
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-07-23 14:27:10 +0900

    Print lists to console

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ const WHITE_ICON =
 const BLACK_ICON =
   'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
-const doAssign = function(t, opts) {
+function doAssign(t, opts) {
   console.log('Someone clicked the button');
-  return t.cards('all').then(function(cards) {
+
+  return t.lists('all').then(function(lists) {
     console.log(JSON.stringify(cards, null, 2));
   });
-};
+}
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, opts) {
