7695e1ba5f4aa2ebfd12b7b47e54b312398b776d
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-07-23 14:11:43 +0900

    Print cards to console

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
-var WHITE_ICON =
+const Promise = window.TrelloPowerUp.Promise;
+
+const WHITE_ICON =
   'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON =
+const BLACK_ICON =
   'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
-var onBtnClick = function(t, opts) {
+const doAssign = function(t, opts) {
   console.log('Someone clicked the button');
+  return t.cards('all').then(function(cards) {
+    console.log(JSON.stringify(cards, null, 2));
+  });
 };
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, opts) {
     return [
       {
-        // we can either provide a button that has a callback function
         icon: {
           dark: WHITE_ICON,
           light: BLACK_ICON
         },
-        text: 'Callback',
-        callback: onBtnClick,
+        text: '割り当てを実行する！！！',
+        callback: doAssign,
         condition: 'edit'
-      },
-      {
-        // or we can also have a button that is just a simple url
-        // clicking it will open a new tab at the provided url
-        icon: {
-          dark: WHITE_ICON,
-          light: BLACK_ICON
-        },
-        text: 'URL',
-        condition: 'always',
-        url: 'https://trello.com/inspiration',
-        target: 'Inspiring Boards' // optional target for above url
       }
     ];
   }
