fb49b7198e93f370770a6763d775269fa54358f4
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-07-23 17:14:12 +0900

    Create a new card

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ const WHITE_ICON =
 const BLACK_ICON =
   'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
+function authorize() {
+  return new Promise((resolve, reject) => {
+    Trello.authorize({
+      type: 'popup',
+      name: 'hawaku-trello',
+      scope: {
+        write: true
+      },
+      expiration: 'never',
+      success: resolve,
+      error: () => {
+        console.log('Authentication failed');
+        reject();
+      }
+    });
+  });
+}
+
 function doAssign(t, opts) {
   console.log('doAssign start');
 
@@ -13,25 +31,16 @@ function doAssign(t, opts) {
   //   console.log(JSON.stringify(lists, null, 2));
   // });
 
-  console.log('authorized', Trello.authorized());
-
-  Trello.authorize({
-    type: 'popup',
-    name: 'hawaku-trello',
-    scope: {
-      read: 'true',
-      write: 'true'
-    },
-    expiration: 'never',
-    success: () => {
-      console.log('Authentication succeeded');
-    },
-    error: () => {
-      console.log('Authentication failed');
-    }
+  return authorize().then(() => {
+    return new Promise((resolve, reject) =>
+      Trello.post(
+        'cards',
+        { idList: '5b5165fe95d13ee66c371780', name: 'new card!' },
+        resolve,
+        reject
+      )
+    );
   });
-
-  console.log('doAssign end');
 }
 
 window.TrelloPowerUp.initialize({
